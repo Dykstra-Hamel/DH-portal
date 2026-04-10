99b35e5cb5f5d88e768b320a7b5c3a1dbecc1b2c
Austin Cox <austin@dykstrahamel.com>
2026-04-10 16:04:06 -0400

    update pwa plugin

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from 'next';
-import withPWA from 'next-pwa';
+import withPWA from '@ducanh2912/next-pwa';
 
 const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
@@ -67,7 +67,5 @@ const nextConfig: NextConfig = {
 
 export default withPWA({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development' || !!process.env.VERCEL,
-  register: true,
-  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 })(nextConfig);
