262b0e84837053d4a41ded2bd5127d00b8b97914
jason-sprague <jason@dykstrahamel.com>
2026-04-15 09:24:10 -0400

    updates to map UI

--- src/components/FieldMap/ServiceWizard/ServiceWizard.tsx
+++ src/components/FieldMap/ServiceWizard/ServiceWizard.tsx
@@ -335,10 +335,10 @@ export function ServiceWizard({ stopId }: ServiceWizardProps) {
           type="button"
           className={styles.backBtn}
           onClick={() => setShowExitConfirm(true)}
-          aria-label="Go back"
+          aria-label="Close"
         >
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-            <path d="M19 12H5M5 12l7 7M5 12l7-7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none" aria-hidden="true">
+            <path d="M13 1L1 13M1 1L13 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         </button>
         <div className={styles.headerGrid}>
